d97c59805e95498dc633d64a38b5e77c46d7a0b9
amirben113 <amir.bennabi@gmail.com>
2023-11-25 19:46:15 +0100

    delete serverWorker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,11 @@ import { BlogComponent } from './components/blog/blog.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { LatestBlogPostsComponent } from './components/latest-blog-posts/latest-blog-posts.component';
-import { BoardComponent } from './components/board/board.component'
+import { BoardComponent } from './components/board/board.component';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Page404Component } from './components/page404/page404.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 
@@ -68,12 +66,6 @@ import { MatSelectModule } from '@angular/material/select';
     MatCardModule,
     ReactiveFormsModule,
     NgxPaginationModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
     MatFormFieldModule,
     MatSelectModule,
     MatProgressSpinnerModule,
@@ -91,7 +83,7 @@ export class AppModule {
       faFacebook,
       faInstagram,
       faTwitter,
-      faYoutube,
+      faYoutube
     );
   }
 }
